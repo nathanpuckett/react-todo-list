108116885c2464501d398c685c5e2118737385ff
Nathan Puckett <nathanfpuckett@gmail.com>
2024-04-24 09:22:44 -0500

    Add toggleTodo & deleteTodo functions

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,26 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ]
     })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -31,10 +51,14 @@ export default function App() {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
         })}
